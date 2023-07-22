b86034c4f1075ef334ce1e56a99fb0c4d84418f2
Amr Elaraby <amrselaraby@gmail.com>
2023-07-22 17:41:53 +0300

    finished user form registeration with create_user

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,32 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponse
+from .forms import UserForm
+from .models import User
 
-# Create your views here.
+
+def register_user(request):
+    if request.method == "POST":
+        print(request.POST)
+        form = UserForm(request.POST)
+        if form.is_valid():
+            # Creating user using the form
+            # password = form.cleaned_data["password"]
+            # user = form.save(commit=False)
+            # user.set_password(password)
+            # user.role = User.CUSTOMER
+            # user.save()
+
+            # Creating user using create_user method
+            del form.cleaned_data["confirm_password"]
+            user = User.objects.create_user(**form.cleaned_data)
+            user.role = User.CUSTOMER
+            user.save()
+            print("User is created")
+            return redirect("register_user")
+
+    else:
+        form = UserForm()
+        context = {
+            "form": form,
+        }
+        return render(request, "accounts/register_user.html", context)
--- foodonline_main/settings.py
+++ foodonline_main/settings.py
@@ -130,6 +130,10 @@ STATICFILES_DIRS = [
     "foodonline_main/static",
 ]
 
+# Media Config
+MEDIA_URL = "media/"
+MEDIA_ROOT = BASE_DIR / "media"
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
