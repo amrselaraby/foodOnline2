e2dfdd1f10eb0ed168113f1f925a43eaca68f4d1
Amr Elaraby <amrselaraby@gmail.com>
2023-07-31 04:34:58 +0300

    vendor registeration

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+
+from vendor.forms import VendorForm
 from .forms import UserForm
-from .models import User
+from .models import User, UserProfile
 from django.contrib import messages
 
 
@@ -36,5 +38,36 @@ def register_user(request):
 
 
 def register_vendor(request):
-    context = {}
+    if request.method == "POST":
+        # STORE THE DATA AND CREATE THE USER
+        form = UserForm(request.POST)
+        v_form = VendorForm(request.POST, request.FILES)
+        if form.is_valid() and v_form.is_valid():
+            # Creating user using create_user method
+            del form.cleaned_data["confirm_password"]
+            user = User.objects.create_user(**form.cleaned_data)
+            user.role = User.VENDOR
+            user.save()
+            vendor = v_form.save(commit=False)
+            vendor.user = user
+            user_profile = UserProfile.objects.get(user=user)
+            vendor.user_profile = user_profile
+            vendor.save()
+            messages.success(
+                request,
+                "Your account has been created successfully!, Please wait for the approval",
+            )
+            return redirect("register_vendor")
+        else:
+            print("invalid forms")
+            print(form.errors)
+            print(v_form.errors)
+    else:
+        form = UserForm()
+        v_form = VendorForm()
+
+    context = {
+        "form": form,
+        "v_form": v_form,
+    }
     return render(request, "accounts/register_vendor.html", context)
